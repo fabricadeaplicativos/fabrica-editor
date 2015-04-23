89e4299d68b1481479b4858289b50c2179d6e67f
Simon <simonxd@gmail.com>
2015-04-23 14:07:43 -0300

    working as module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var path = require('path');
 
 // external deps
+var _ = require('lodash');
 
 // internal deps
 var createMarkedHtmlServer = require('./lib/create-marked-html-server'),
@@ -9,51 +10,99 @@ var createMarkedHtmlServer = require('./lib/create-marked-html-server'),
 	createBracketsServer = require('./lib/create-brackets-server'),
 	createDeploydServer = require('./lib/create-deployd-server');
 
-// global options
-var globalOptions = {
-	projectsDir: path.join(__dirname, 'web')
+
+
+// PORTS at which each server will work
+var PORTS = {
+	markedHtmlServer: 3100,
+	bracketsServer: 3101,
+	socketServer: 3102,
+	deploydServer: 3103
+};
+
+function startEditor(options) {
+
+	if (!options.projectsDir) {
+		throw new Error('"projectsDir" option is required :)')
+	}
+
+	/////////////////////
+	// Create the marked html server
+	createMarkedHtmlServer({
+		port: PORTS.markedHtmlServer,
+		
+		root: options.projectsDir,
+		xPathAttribute: 'data-x-path',
+		fnameAttribute: 'data-fname',
+
+		// stylesheets to be injected
+		injectStylesheets: [
+			// test injection
+			'http://localhost:' + PORTS.socketServer + '/assets/test.css',
+			// context menu
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/jQuery-contextMenu/src/jquery.contextMenu.css'
+		],
+
+		// scripts to be injected
+		injectScripts: [
+			// test injection
+			'http://localhost:' + PORTS.socketServer + '/assets/test.js',
+
+			// jquery and lodash
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/jquery/dist/jquery.js',
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/lodash/lodash.js',
+			
+			// contextmenu
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/jQuery-contextMenu/src/jquery.ui.position.js',
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/jQuery-contextMenu/src/jquery.contextMenu.js',
+
+			// socket.io
+			'http://localhost:' + PORTS.socketServer + '/assets/bower_components/socket.io-client/socket.io.js',
+			// domlight
+			'http://localhost:' + PORTS.socketServer + '/assets/node_modules/domlight/domlight.js',
+
+			// socket connection
+			'http://localhost:' + PORTS.socketServer + '/assets/canvas-socket-connection.js'
+		]
+	});
+
+
+
+	/////////////////
+	// Create the brackets editor server
+	createBracketsServer({
+		port: PORTS.bracketsServer,
+		projectsDir: options.projectsDir,
+		supportDir: path.join(__dirname, 'support'),
+	});
+
+	/////////////////
+	// Create the socket server
+	createSocketServer({
+		port: PORTS.socketServer,
+
+		socketNamespace: '/canvas',
+
+		// global
+		projectsDir: options.projectsDir
+	});
+
+
+	// NEEDS IMPROVEMENT: deployd server
+
+	// createDeploydServer({
+	// 	port: PORTS.deploydServer,
+	// 	env: 'development', 
+	// 	db: {
+	// 		host: 'localhost',
+	// 		port: 27017,
+	// 		name: 'dpd'
+	// 	}
+	// });
 }
 
-
-/////////////////////
-// Create the marked html server
-createMarkedHtmlServer({
-	port: 3000,
-	
-	root: globalOptions.projectsDir,
-	xPathAttribute: 'data-x-path',
-	fnameAttribute: 'data-fname'
-});
-
-/////////////////
-// Create the socket server
-createSocketServer({
-	port: 4000,
-
-	socketNamespace: '/canvas',
-
-	// global
-	projectsDir: globalOptions.projectsDir
-});
+// export
+module.exports = startEditor;
 
 
-/////////////////
-// Create the brackets editor server
-createBracketsServer({
-	port: 8000,
-	projectsDir: path.join(__dirname, 'web'),
-	supportDir: path.join(__dirname, 'web/support'),
 
-	// global
-	projectsDir: globalOptions.projectsDir
-});
-
-createDeploydServer({
-	port: 3051,
-	env: 'development', 
-	db: {
-		host: 'localhost',
-		port: 27017,
-		name: 'dpd'
-	}
-});
